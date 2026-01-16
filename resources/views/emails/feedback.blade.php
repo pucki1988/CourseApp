678347f4b2b527a5894a17f914b5c51adb96c457
cwm-a-schuster <a.schuster@cw-mobile.de>
2026-01-16 15:32:22 +0100

    Update feedback.blade.php

--- resources/views/emails/feedback.blade.php
+++ resources/views/emails/feedback.blade.php
@@ -4,9 +4,11 @@
     <p><strong>Name:</strong> {{ $feedbackName }}</p>
 @endif
 
-<p><strong>Nachricht:</strong></p>
-<p>{{ $feedbackMessage }}</p>
-
 @if(!empty($feedbackEmail))
     <p><strong>Absender:</strong> {{ $feedbackEmail }}</p>
 @endif
+
+<p><strong>Nachricht:</strong></p>
+<p>{{ $feedbackMessage }}</p>
+
+
