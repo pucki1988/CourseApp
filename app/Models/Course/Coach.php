b06771a6ec3fad1f75c9c26d64b7b0f0bd8f4ebd
cwm-a-schuster <a.schuster@cw-mobile.de>
2026-03-04 14:24:26 +0100

    Update Coach.php

--- app/Models/Course/Coach.php
+++ app/Models/Course/Coach.php
@@ -28,7 +28,7 @@ class Coach extends Model
 
     public function user()
     {
-        return $this->belongsTo(User::class);
+        return $this->belongsTo(User::class, 'user_id');
     }
 
     // Optional: Coach ↔ Kurse
